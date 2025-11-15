6fbd84abddc7b54e056fe7c617a6588100f44e9d
TH8359 <hogue73@embarqmail.com>
2025-11-15 09:21:32 -0500

    Fixed the exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,12 +1,7 @@
 ﻿namespace Classes
 {
 
-    public class Car
-    {
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-    }
+    
     
     public class Program
     {
